0feee7305a53db5de28066a7094227c3baa18b83
DominicDev <s0l0estudi4r@gmail.com>
2025-12-27 10:37:41 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'MGMT';
+export let nameSongs = [
+	"TIME TO PRETEND", /*1*/
+	"KIDS", /*2*/
+	"LITTLE DARK AGE", /*3*/
+	"ELECTRIC FEEL", /*4*/
+	"ME AND MICHAEL", /*5*/
+	"TSLAMP", /*6*/
+	"SHE WORKS OUT TOO MUCH", /*7*/
+	"DAYS THAT GOT AWAY", /*8*/
+	"WHEN YOU’RE SMALL", /*9*/
+	"ONE THING LEFT TO TRY", /*10*/
+	"JAMES", /*11*/
+	"SONG FOR DAN TREACY", /*12*/
+	"IT'S WORKING", /*13*/
+	"CONGRATULATIONS", /*14*/
+	"FLASH DELIRIUM", /*15*/
+	"BRIAN ENO", /*16*/
+	" SIBERIAN BREAKS", /*17*/
+	"METANOIA", /*18*/
+	"MYSTERY DISEASE", /*19*/
+	"INSTRONSPECTION", /*20*/
+]
